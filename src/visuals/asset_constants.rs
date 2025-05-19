bc41a725b379e2bce431199a7fb806bd4b2a8ab2
Erica Abenti <emmert.math@gmail.com>
2025-05-18 22:36:36 -0500

    envoy sprites

--- src/visuals/asset_constants.rs
+++ src/visuals/asset_constants.rs
@@ -1,3 +1,4 @@
 use std::str;
 
 pub const MAP_MOVABLE_PLAYER_CHARACTER_MYSTIC : &str = "characters/mystic-animation.png";
+pub const MAP_MOVABLE_PLAYER_CHARACTER_ENVOY : &str = "characters/envoy-animation.png";
